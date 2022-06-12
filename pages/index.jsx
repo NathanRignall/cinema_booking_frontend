67d6f0d9d8ac1b96d9b528e0595a05a4f99a9016
Nathan Rignall <rignallnathan@gmail.com>
2022-06-12 21:46:54 +0100

    update index page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,7 @@
 import Layout from "../components/layouts/anonymous";
 
+import Link from "next/link";
+
 import { Spinner, Card, Col, Row, Nav, Button } from "react-bootstrap";
 
 // main app function
@@ -9,15 +11,19 @@ export default function Main() {
       <div className="px-4 py-5 my-5 text-center">
         <h1 className="display-5 fw-bold">XL Cinema</h1>
         <div className="col-lg-6 mx-auto">
-          <p className="lead mb-4">
-            Quickly design and customize responsive mobile-first sites with
-            Bootstrap, the world’s most popular front-end open source toolkit,
-            featuring Sass variables and mixins, responsive grid system,
-            extensive prebuilt components, and powerful JavaScript plugins.
-          </p>
+          <p className="lead mb-4">Welcome to our online booking site!</p>
           <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
-            <Button variant="dark" size="lg">Book Seats</Button>
-            <Button variant="outline-dark" size="lg">Account</Button>
+            <Link href={`/movie`} passHref>
+              <Button variant="dark" size="lg">
+                Book Seats
+              </Button>
+            </Link>
+
+            <Link href={`/account`} passHref>
+              <Button variant="outline-dark" size="lg">
+                Account
+              </Button>
+            </Link>
           </div>
         </div>
       </div>
